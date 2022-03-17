95a5c99e2fc79ca0079119ca12bad09ed0dfe6b2
Jakub Rożek <jakub@stoplight.io>
2022-03-17 16:58:04 +0100

    feat: introduce IShareableNode

--- src/graph.ts
+++ src/graph.ts
@@ -1,5 +1,8 @@
-export interface INode {
+export interface IShareableNode {
   id: string;
+}
+
+export interface INode extends IShareableNode {
   /** An internal identifier. For example, the operationId property in OAS. */
   iid?: string;
   tags?: INodeTag[];
